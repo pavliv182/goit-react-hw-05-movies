a255397ba290de1ba68f3f2f53bd959f763456e1
pavliv182 <97523241+pavliv182@users.noreply.github.com>
2022-10-16 14:36:03 +0000

    Deploying to gh-pages from @ pavliv182/goit-react-hw-05-movies@3f4eaaf2407bc74893fd579fd69cf8ee5147f8e0 🚀

---
+++ static/js/60.792ae573.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[60],{794:function(){},60:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return c.a}});var u=n(794),c=n.n(u)}}]);
